5f5fba181fc5d0d5825a9303b99261ea0f04ebd3
Mike Fix <mrfix84@gmail.com>
2017-07-15 19:39:05 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const DEFAULT_OPTIONS = {
+  types: {},
+  objDelimeter: '.',
+  mapDelimeter: ':',
+  preFilters: [],
+  postFilters: []
+}
+
+const _createRootObj = (obj, key) => isNaN(parseInt(key, 10))
+  ? {}
+  : Array(parseInt(key, 10))
+
+const assign = (key, delimeter = DEFAULT_OPTIONS.objDelimeter) => (obj, value) => {
+  key.split(delimeter).reduce((accum, key, i, array) => {
+    if (i === array.length - 1) accum[key] = value
+    else if (!accum[key]) accum[key] = _createRootObj(accum, array[i + 1])
+    return accum[key]
+  }, obj)
+
+  return obj
+}
+
+const get = (key, delimeter = DEFAULT_OPTIONS.objDelimeter) => obj =>
+  key.split(delimeter).reduce((accum, key) => accum ? accum[key] : undefined, obj)
+
+const compose = (...fns) => (res, ...args) =>
+  fns.reverse().reduce((accum, next) => next(accum, ...args), res)
+
+const _getMapSpec = (mapping, delimeter) => (
+  mapping.indexOf(delimeter) > -1
+    ? mapping
+    : mapping + delimeter + mapping)
+    .split(delimeter)
+
+const _normalizeMapping = mapping => typeof mapping === 'string'
+  ? { field: mapping }
+  : mapping
+
+const _getMappingFilter = (mapping, types) => {
+  if (mapping.type) {
+    if (Array.isArray(mapping.type)) return compose(...mapping.type)
+    if (typeof mapping.type === 'function') return mapping.type
+    if (types.hasOwnProperty(mapping.type)) return types[mapping.type]
+  }
+  return _ => _
+}
+
+class Mapper {
+  constructor (options) {
+    const config = Object.assign(DEFAULT_OPTIONS, options)
+    this.config = config
+  }
+
+  map (mappings, curr, next = {}) {
+    const options = this.config
+    return mappings
+      .map(_normalizeMapping)
+      .reduce((accum, mapping) => {
+        const [alphaField, betaField] = _getMapSpec(mapping.field, options.mapDelimeter)
+
+        const filter = compose(
+          ...options.postFilters,
+          _getMappingFilter(mapping, options.types),
+          ...options.preFilters
+        )
+        const value = filter(get(alphaField, options.objDelimeter)(curr), mapping, options)
+        return value === undefined
+          ? accum
+          : assign(betaField, options.objDelimeter)(accum, value)
+      }, next)
+  }
+}
+
+Mapper.get = get
+Mapper.assign = assign
+
+exports.default = Mapper
+module.exports = Mapper
---
+++ test/index.spec.js
@@ -0,0 +1,151 @@
+const Mapper = require('..')
+
+const mapper = new Mapper()
+
+it('should deeply get object fields', () => {
+  const deepObjectNesting = {
+    really: {
+      really: {
+        really: {
+          deep: 'deep'
+        }
+      }
+    }
+  }
+  expect(Mapper.get('really.really.really.deep')(deepObjectNesting)).toEqual('deep')
+})
+
+it('should deeply get object fields', () => {
+  const shallowObj = {
+    shallow: false
+  }
+
+  const deepify = Mapper.assign('really.really.deep')
+
+  expect(deepify(shallowObj, true).really.really.deep).toEqual(true)
+})
+
+it('should be isomorphic', () => {
+  const complexKey = 'a.b.c.d.e.f'
+  const expected = 'ABCDEF'
+
+  const get = Mapper.get(complexKey)
+  const set = Mapper.assign(complexKey)
+
+  expect(get(set({}, expected))).toEqual(expected)
+})
+
+it('should work with basic string mappings', () => {
+  const curr = {
+    map: true,
+    morph: false
+  }
+
+  const mappings = [ 'map', 'morph' ]
+
+  const mapped = mapper.map(mappings, curr)
+
+  expect(mapped.map).toEqual(true)
+  expect(mapped.morph).toEqual(false)
+})
+
+it('should filter unmapped values', () => {
+  const curr = {
+    name: 'Mike',
+    email: 'hi@dawnlabs.io',
+    password: 'throw'
+  }
+
+  const mappings = [ 'name', 'email' ]
+  const mapped = mapper.map(mappings, curr)
+
+  expect(mapped).toEqual({ name: 'Mike', email: 'hi@dawnlabs.io' })
+})
+
+it('should handle deep transformations', () => {
+  const curr = {
+    here: {
+      there: {
+        anywhere: true
+      }
+    },
+    now: false
+  }
+
+  const mappings = [
+    'here.there.anywhere:here',
+    'now:maybe.later.never'
+  ]
+
+  expect(mapper.map(mappings, curr).here).toEqual(true)
+  expect(mapper.map(mappings, curr).maybe.later.never).toEqual(false)
+})
+
+it('should allow function composition for creating types', () => {
+  const curr = { binary: '0001 0001' }
+
+  const ENUM = {
+    0: '$',
+    1: '£',
+    2: '₩',
+    17: '¥'
+  }
+
+  // contrived type
+  const currencyEnumFromBinary = [
+    v => ENUM[v] || '$',
+    v => parseInt(v, 2),
+    v => v.replace(/\s+/g, '')
+  ]
+  const mappings = [{
+    field: 'binary:decimal',
+    type: currencyEnumFromBinary
+  }]
+
+  expect(mapper.map(mappings, curr).decimal).toEqual('¥')
+})
+
+it('should allow you to pass pre-mapping filters', () => {
+  const curr = { possiblyNull: null }
+  const USE_DEFAULT = (value, field) => value == null
+    ? field.default
+    : value
+
+  const mappings = [{
+    field: 'possiblyNull:definitelyNotNull',
+    default: 'NOT TODAY!'
+  }]
+
+  const preFilterMapper = new Mapper({
+    preFilters: [USE_DEFAULT]
+  })
+
+  const mapped = preFilterMapper.map(mappings, curr)
+  expect(mapped.definitelyNotNull).toEqual('NOT TODAY!')
+})
+
+it('should allow you to pass post-mapping filters', () => {
+  const curr = { possiblyNull: null }
+  const USE_GLOBAL_DEFAULTS = (value, field, options) => {
+    if (value == null){
+      return options.types && options.GLOBAL_DEFAULTS[field.type]
+    }
+    return value
+  }
+
+  const GLOBAL_DEFAULTS = {
+    nonNullable: 'THIS IS JUST A GLOBAL DEFAULT'
+  }
+
+  const mappings = [{
+    field: 'possiblyNull:definitelyNotNull',
+    type: 'nonNullable'
+  }]
+
+  const postFilterMapper = new Mapper({
+    postFilters: [USE_GLOBAL_DEFAULTS],
+    GLOBAL_DEFAULTS
+  })
+
+  expect(postFilterMapper.map(mappings, curr).definitelyNotNull).toEqual('THIS IS JUST A GLOBAL DEFAULT')
+})
